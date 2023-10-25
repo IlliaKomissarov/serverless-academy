4fb23a01a678c4f5603d03cedf0f589bae598f97
Illia Komissarov <iliakomissarow99@gmail.com>
2023-10-25 20:13:30 +0300

    all done

--- 03_cli_telegram_console_sender/app.js
+++ 03_cli_telegram_console_sender/app.js
@@ -5,19 +5,19 @@ const program = new Command();
 program.name("app").version("0.1.3");
 
 const BOT_TOKEN =
-  (process.env.TELEGRAM_BOT_TOKEN && process.env.TELEGRAM_BOT_TOKEN.trim()) ||
-  "6745222833:AAEYvEsQ9AKtOr-cssbtaQLLGWqmd2qLfMs";
-const CHAT_ID = process.env.TELEGRAM_CHAT_ID || "474673236";
+  process.env.TELEGRAM_BOT_TOKEN?.trim() || "TELEGRAM_BOT_TOKEN";
+const CHAT_ID = process.env.TELEGRAM_CHAT_ID || "CHAT_ID";
 
 const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 
 async function sendMsg(msg) {
   try {
-    await bot.sendMessage(CHAT_ID, msg);
+      await bot.sendMessage(CHAT_ID, msg);
+      console.log("You successfully sent the message to your Telegram Bot.");
     process.exit(0);
   } catch (error) {
-    console.log(error.code);
-    console.log(error.response.body);
+    console.error(error.code);
+    console.error(error.response.body);
     process.exit(0);
   }
 }
@@ -25,11 +25,11 @@ async function sendMsg(msg) {
 async function sendPhoto(photoUrl) {
   try {
     await bot.sendPhoto(CHAT_ID, photoUrl);
-    console.log("You successfully sent the photo to your bot.");
+    console.log("You successfully sent the photo to your Telegram Bot.");
     process.exit(0);
   } catch (error) {
-    console.log(error.code);
-    console.log(error.response.body);
+    console.error(error.code);
+    console.error(error.response.body);
     process.exit(0);
   }
 }
@@ -39,7 +39,7 @@ program
   .alias("m")
   .alias("message")
   .argument("<message>")
-  .description("Send message to Telegram Bot.")
+  .description("Send message to the Telegram Bot.")
   .action(sendMsg);
 
 program
@@ -48,7 +48,7 @@ program
   .alias("photo")
   .argument("<path>")
   .description(
-    "Send photo to Telegram Bot. Just drag and drop it console after p-flag."
+    "Send photo to the Telegram Bot. Just drag and drop it console after p-flag."
   )
   .action(sendPhoto);
 
