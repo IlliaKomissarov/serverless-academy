8408447f00557ed29a3538f7124f80774bd9c2cb
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-01 17:23:29 +0200

    task 6 started

--- 05-telegram-bot-exchange-rates/app.js
+++ 05-telegram-bot-exchange-rates/app.js
@@ -3,8 +3,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const { initializeBot } = require("./menu");
 const { fetchExchangeRates } = require("./api");
 const TELEGRAM_BOT_TOKEN =
-  process.env.TELEGRAM_BOT_TOKEN ||
-  "6745222833:AAEYvEsQ9AKtOr-cssbtaQLLGWqmd2qLfMs";
+  process.env.TELEGRAM_BOT_TOKEN || "TELEGRAM_BOT_TOKEN";
 
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 
