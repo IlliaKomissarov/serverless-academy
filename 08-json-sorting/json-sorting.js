aaae735d92f9d477b749f019fd418fb0b1d33d3a
Illia Komissarov <iliakomissarow99@gmail.com>
2023-11-02 23:42:33 +0200

    Update json-sorting.js

--- 08-json-sorting/json-sorting.js
+++ 08-json-sorting/json-sorting.js
@@ -1,42 +1,36 @@
 const axios = require("axios");
 const { endpoints } = require("./endpoints");
 
-const getRequest = async (url, reqCount = 0) => {
-  if (reqCount >= 3) {
-    return ["Fail"];
-  }
-  reqCount += 1;
+const getRequest = async (url) => {
   try {
     const { data } = await axios.get(url);
-    if (data) {
-      return ["Success", data];
-    }
+    return ["Success", data];
   } catch (error) {
     return ["Fail"];
   }
-  return getRequest(url, reqCount);
 };
 
 const sortJSON = async () => {
-  let isDone = { true: 0, false: 0 };
+  const isDone = { true: 0, false: 0 };
 
-  for (const endpoint of endpoints) {
+  const requests = endpoints.map(async (endpoint) => {
     const [status, data] = await getRequest(endpoint);
     let msg = "The endpoint is unavailable.";
 
     if (data && data.isDone) {
       msg = `isDone - ${data.isDone}`;
       isDone[data.isDone] += 1;
-      console.log(`[${status}] ${endpoint}: ${msg}`);
     }
 
     console.log(`[${status}] ${endpoint}: ${msg}`);
-  }
+    return status;
+  });
+
+  const results = await Promise.all(requests);
 
   console.log(
     `\nFound True values: ${isDone.true},\nFound False values: ${isDone.false}`
   );
-  return;
 };
 
 sortJSON();
